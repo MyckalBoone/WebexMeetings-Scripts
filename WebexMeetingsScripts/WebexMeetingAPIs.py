11b9015ce212f211b63c5205293a1339e35bf59b
MyckalBoone <myckalgb95@gmail.com>
2023-05-30 23:34:10 +0000

    First WebexMeetings Commit

---
+++ WebexMeetingsScripts/WebexMeetingAPIs.py
@@ -0,0 +1,11 @@
+# XML API Support webex meeting services -> detailed user management comprehensive scheduling features, attendee mannagement and reporting
+
+# Pre register API key -> "api.webex.com/gapi/registerapikey" -> then use HM256 to generate JWT token
+
+# URL = "https://api.Webex.com/WBXService/XMLService"	# "POST" with XML body -> Creating or scheduling Webex Meeting
+
+# URL = "https://api.Webex.com/WBXService/XMLService"	# "POST" with XML body -> List all Webex Meetings that meet criteria
+
+# URL = "https://api.Webex.com/WBXService/XMLService"	# "POST" with XML body -> update Webex Meeting attributes such as attendes, record meeting, schedules, and media support
+
+# URL = "https://api.Webex.com/WBXService/XMLService"	# "DELETE" with XML body -> Creating or scheduling Webex Meeting
